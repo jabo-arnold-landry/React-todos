27afd4a91b04615c889c29684d932a9d3faa68e9
jabo <arnoldjabo@gmail.com>
2026-04-06 14:49:20 +0200

    feat:Add a todo component that will render single todos

---
+++ src/component/Todo.tsx
@@ -0,0 +1,10 @@
+function Todo() {
+  return (
+    <>
+      <label htmlFor="task-1">taskOne</label>
+      <input type="checkbox" name="task-checkbox" id="task-1" />
+    </>
+  );
+}
+
+export default Todo;
