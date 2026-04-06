e19a40311b034227e62e4fd7b502bfb8359f7668
jabo <arnoldjabo@gmail.com>
2026-04-06 15:44:03 +0200

    feat:Add the funcionaliy to get the user todos and also handled an edge case when the input is empty

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
+import { useActionState, useState } from "react";
 import Todo from "./component/Todo";
+import { type TodoStructure } from "./assets/types";
 
 function App() {
+  const [todos, setTodos] = useState<TodoStructure[]>([]);
+  const [_, actionToTrigger, isPending] = useActionState(addTodo, null);
+
+  function addTodo(_: unknown, formData: FormData): unknown {
+    const task = formData.get("task") as string;
+    if (!task) return alert("can not set empty todo please add one");
+    const obj = { isDone: false, task };
+    setTodos((prev) => [...prev, obj]);
+  }
+
   return (
     <>
-      <Todo />
+      <form action={actionToTrigger}>
+        <input
+          type="text"
+          name="task"
+          id="input"
+          placeholder="Add Todo..."
+          disabled={isPending}
+        />
+
+        <button> +</button>
+      </form>
+
+      <main>
+        {todos.length !== 0 ? (
+          todos.map((task, index) => {
+            return (
+              <div key={index}>
+                <Todo {...task} />
+              </div>
+            );
+          })
+        ) : (
+          <p>No tasks</p>
+        )}
+      </main>
     </>
   );
 }
